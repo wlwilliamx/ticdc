cd46264cc0aa20f11a9e89f8099b7c9e4d43d4ca
hongyunyan <649330952@qq.com>
2025-01-21 14:13:11 +0800

    Fix unstable test `ddl-attribute` (#924)
    close pingcap/ticdc#925

--- tests/integration_tests/ddl_attributes/run.sh
+++ tests/integration_tests/ddl_attributes/run.sh
@@ -43,33 +43,33 @@ function run() {
 	sleep 3
 	check_table_exists ddl_attributes.attributes_t1_new ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}
 	check_table_exists ddl_attributes.finish_mark ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}
+	
+
+	ensure 30 "run_sql 'show create table ddl_attributes.placement_t1;' ${UP_TIDB_HOST} ${UP_TIDB_PORT}"
+	ensure 30 "run_sql 'show create table ddl_attributes.placement_t2;' ${UP_TIDB_HOST} ${UP_TIDB_PORT}"
+
+	ensure 30 "run_sql 'show create table ddl_attributes.placement_t1;' ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} &&
+		check_contains 'CREATE TABLE \`placement_t1\`' "
+	ensure 30 "run_sql 'show create table ddl_attributes.placement_t2;' ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} &&
+		check_contains 'CREATE TABLE \`placement_t2\`' "
+
+	CREATE_TTL_SQL_CONTAINS1='/*T![ttl] TTL=\`t\` + INTERVAL 1 DAY */ /*T![ttl] TTL_ENABLE='OFF' */ /*T![ttl] TTL_JOB_INTERVAL='1h' */'
+	CREATE_TTL_SQL_CONTAINS2='/*T![ttl] TTL=\`t\` + INTERVAL 1 DAY */ /*T![ttl] TTL_ENABLE='OFF' */ /*T![ttl] TTL_JOB_INTERVAL='7h' */'
+
+	ensure 30 "run_sql 'show create table ddl_attributes.ttl_t1;' ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} &&
+		check_contains $CREATE_TTL_SQL_CONTAINS1"
+	ensure 30 "run_sql 'show create table ddl_attributes.ttl_t2;' ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} &&
+		check_contains $CREATE_TTL_SQL_CONTAINS1"
+	ensure 30 "run_sql 'show create table ddl_attributes.ttl_t3;' ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} &&
+		check_contains $CREATE_TTL_SQL_CONTAINS1"
+	ensure 30 "run_sql 'show create table ddl_attributes.ttl_t4;' ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} &&
+		check_not_contains 'TTL_ENABLE' &&
+		check_not_contains 'TTL='"
+	ensure 30 "run_sql 'show create table ddl_attributes.ttl_t5;' ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} &&
+		check_contains $CREATE_TTL_SQL_CONTAINS2"
+
 	check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml
-
 	cleanup_process $CDC_BINARY
-
-	run_sql "show create table ddl_attributes.placement_t1;" ${UP_TIDB_HOST} ${UP_TIDB_PORT}
-	run_sql "show create table ddl_attributes.placement_t2;" ${UP_TIDB_HOST} ${UP_TIDB_PORT}
-
-	run_sql "show create table ddl_attributes.placement_t1;" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} &&
-		check_contains "CREATE TABLE \`placement_t1\` "
-	run_sql "show create table ddl_attributes.placement_t2;" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} &&
-		check_contains "CREATE TABLE \`placement_t2\` "
-
-	TTL_MARK='![ttl]'
-	CREATE_TTL_SQL_CONTAINS1="/*T${TTL_MARK} TTL=\`t\` + INTERVAL 1 DAY */ /*T${TTL_MARK} TTL_ENABLE='OFF' */ /*T${TTL_MARK} TTL_JOB_INTERVAL='1h' */"
-	CREATE_TTL_SQL_CONTAINS2="/*T${TTL_MARK} TTL=\`t\` + INTERVAL 1 DAY */ /*T${TTL_MARK} TTL_ENABLE='OFF' */ /*T${TTL_MARK} TTL_JOB_INTERVAL='7h' */"
-
-	run_sql "show create table ddl_attributes.ttl_t1;" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} &&
-		check_contains "$CREATE_TTL_SQL_CONTAINS1"
-	run_sql "show create table ddl_attributes.ttl_t2;" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} &&
-		check_contains "$CREATE_TTL_SQL_CONTAINS1"
-	run_sql "show create table ddl_attributes.ttl_t3;" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} &&
-		check_contains "$CREATE_TTL_SQL_CONTAINS1"
-	run_sql "show create table ddl_attributes.ttl_t4;" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} &&
-		check_not_contains "TTL_ENABLE" &&
-		check_not_contains "TTL="
-	run_sql "show create table ddl_attributes.ttl_t5;" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} &&
-		check_contains "$CREATE_TTL_SQL_CONTAINS2"
 }
 
 trap stop_tidb_cluster EXIT

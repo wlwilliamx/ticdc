ce9c25e5ebd1322a0d0576eb31584a9f4d7fa4e5
wlwilliamx <wlwilliamx@gmail.com>
2025-08-06 15:11:14 +0800

    modify filter-helper to the new filter in ticdc repo

--- cmd/filter-helper/main.go
+++ cmd/filter-helper/main.go
@@ -18,10 +18,9 @@ import (
 	"strings"
 
 	"github.com/pingcap/ticdc/cmd/util"
+	"github.com/pingcap/ticdc/pkg/config"
+	"github.com/pingcap/ticdc/pkg/filter"
 	timodel "github.com/pingcap/tidb/pkg/meta/model"
-	"github.com/pingcap/tiflow/cdc/model"
-	"github.com/pingcap/tiflow/pkg/config"
-	"github.com/pingcap/tiflow/pkg/filter"
 	"github.com/spf13/cobra"
 )
 
@@ -55,7 +54,7 @@ func runFilter(cmd *cobra.Command, args []string) {
 		fmt.Printf("decode config file error: %v\n", err)
 		return
 	}
-	ft, err := filter.NewFilter(cfg, "")
+	ft, err := filter.NewFilter(config.NewDefaultFilterConfig(), "UTC", false, false)
 	if err != nil {
 		fmt.Printf("filter create error: %v\n", err)
 		return
@@ -73,7 +72,7 @@ func runFilter(cmd *cobra.Command, args []string) {
 
 	switch target {
 	case "table":
-		matched := !ft.ShouldIgnoreTable(tableAndSchema[0], tableAndSchema[1])
+		matched := !ft.ShouldIgnoreTable(tableAndSchema[0], tableAndSchema[1], nil)
 		if matched {
 			fmt.Printf("Table: %s, Matched filter rule\n", table)
 			return
@@ -81,22 +80,7 @@ func runFilter(cmd *cobra.Command, args []string) {
 		fmt.Printf("Table: %s, Not matched filter rule\n", table)
 	case "ddl":
 		ddlType := timodel.ActionCreateTable
-		discard := ft.ShouldDiscardDDL(ddlType, tableAndSchema[0], tableAndSchema[1])
-		if discard {
-			fmt.Printf("DDL: %s, should be discard by event filter rule\n", ddl)
-			return
-		}
-		ignored, err := ft.ShouldSkipDDLEvent(&model.DDLEvent{
-			StartTs: uint64(0),
-			Query:   ddl,
-			Type:    ddlType,
-			TableInfo: &model.TableInfo{
-				TableName: model.TableName{
-					Schema: tableAndSchema[0],
-					Table:  tableAndSchema[1],
-				},
-			},
-		})
+		ignored, err := ft.ShouldIgnoreDDL(tableAndSchema[0], tableAndSchema[1], ddl, ddlType, nil)
 		if err != nil {
 			fmt.Printf("filter ddl error: %s, error: %v\n", ddl, err)
 			return

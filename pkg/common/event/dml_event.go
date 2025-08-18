4415eacaeaf296037d62ea1374dd2b9685d98425
wlwilliamx <wlwilliamx@gmail.com>
2025-08-18 18:15:08 +0800

    remove useless nil check

--- pkg/common/event/dml_event.go
+++ pkg/common/event/dml_event.go
@@ -342,7 +342,7 @@ func (t *DMLEvent) AppendRow(raw *common.RawKVEntry,
 	// begin; insert into t (id) values (1); delete from t where id=1; commit;
 	// Just ignore these row changed events
 	// See https://github.com/pingcap/tiflow/issues/2612 for more details.
-	if (raw.Value == nil && raw.OldValue == nil) || (len(raw.Value) == 0 && len(raw.OldValue) == 0) {
+	if len(raw.Value) == 0 && len(raw.OldValue) == 0 {
 		log.Debug("the value and old_value of the raw kv entry are both nil, skip it", zap.String("raw", raw.String()))
 		return nil
 	}

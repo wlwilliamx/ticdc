32766a280f5031e0a04fe5e02ae76f66721e3c61
hongyunyan <649330952@qq.com>
2025-10-16 10:57:49 +0800

    tests: Fix unstable test `fail_over_ddl_mix_with_syncpoint` (#2668)
    close pingcap/ticdc#2667

--- tests/integration_tests/fail_over_ddl_mix_with_syncpoint/run.sh
+++ tests/integration_tests/fail_over_ddl_mix_with_syncpoint/run.sh
@@ -150,7 +150,7 @@ main() {
 	check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml 500
 
 	checkpoint1=$(cdc cli changefeed query -c "test" 2>&1 | grep -v "Command to ticdc" | jq '.checkpoint_tso')
-	sleep 5
+	sleep 20
 	checkpoint2=$(cdc cli changefeed query -c "test" 2>&1 | grep -v "Command to ticdc" | jq '.checkpoint_tso')
 
 	if [[ "$checkpoint1" -eq "$checkpoint2" ]]; then

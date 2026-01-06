4b1b5af0d814638295ebb4a5795304ea2c8554e7
wlwilliamx <wlwilliamx@gmail.com>
2026-01-06 16:08:32 +0800

    fix retrieve operator but dispatcher not exists

--- downstreamadapter/dispatcherorchestrator/dispatcher_orchestrator.go
+++ downstreamadapter/dispatcherorchestrator/dispatcher_orchestrator.go
@@ -157,18 +157,17 @@ func (m *DispatcherOrchestrator) handleBootstrapRequest(
 	var err error
 	if !exists {
 		start := time.Now()
-		manager, err = dispatchermanager.
-			NewDispatcherManager(
-				req.KeyspaceId,
-				cfId,
-				cfConfig,
-				req.TableTriggerEventDispatcherId,
-				req.TableTriggerRedoDispatcherId,
-				req.StartTs,
-				from,
-				req.IsNewChangefeed,
-			)
-			// Fast return the error to maintainer.
+		manager, err = dispatchermanager.NewDispatcherManager(
+			req.KeyspaceId,
+			cfId,
+			cfConfig,
+			req.TableTriggerEventDispatcherId,
+			req.TableTriggerRedoDispatcherId,
+			req.StartTs,
+			from,
+			req.IsNewChangefeed,
+		)
+		// Fast return the error to maintainer.
 		if err != nil {
 			log.Error("failed to create new dispatcher manager",
 				zap.Any("changefeedID", cfId.Name()), zap.Duration("duration", time.Since(start)), zap.Error(err))
@@ -483,7 +482,10 @@ func retrieveCurrentOperatorsForBootstrapResponse(
 	manager.GetCurrentOperatorMap().Range(func(key, value any) bool {
 		req := value.(dispatchermanager.SchedulerDispatcherRequest)
 		_, ok := manager.GetDispatcherMap().Get(common.NewDispatcherIDFromPB(req.Config.DispatcherID))
-		if !ok {
+		// Log error if dispatcher not found and action is not create
+		// It's possible that the dispatcher is not found when the action is create
+		// because the dispatcher may be created after the operator is stored
+		if !ok && req.ScheduleAction != heartbeatpb.ScheduleAction_Create {
 			log.Error("Dispatcher not found, this should not happen",
 				zap.String("changefeed", changefeedID.String()),
 				zap.String("dispatcherID", req.Config.DispatcherID.String()),
@@ -507,7 +509,10 @@ func retrieveRedoCurrentOperatorsForBootstrapResponse(
 	manager.GetRedoCurrentOperatorMap().Range(func(key, value any) bool {
 		req := value.(dispatchermanager.SchedulerDispatcherRequest)
 		_, ok := manager.GetRedoDispatcherMap().Get(common.NewDispatcherIDFromPB(req.Config.DispatcherID))
-		if !ok {
+		// Log error if dispatcher not found and action is not create
+		// It's possible that the dispatcher is not found when the action is create
+		// because the dispatcher may be created after the operator is stored
+		if !ok && req.ScheduleAction != heartbeatpb.ScheduleAction_Create {
 			log.Error("Redo dispatcher not found, this should not happen",
 				zap.String("changefeed", changefeedID.String()),
 				zap.String("dispatcherID", req.Config.DispatcherID.String()),
